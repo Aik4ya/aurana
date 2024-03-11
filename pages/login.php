5c9f8d5acc69b5257a420cd424b8159c69305646
root <root@vps-51e86af9.vps.ovh.net>
2024-03-11 14:19:27 +0000

    BackUp Aurana

---
+++ pages/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de Connexion</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/login.css">
+    
+</head>
+<body>
+    <section id="hero" class="d-flex align-items-center">
+        <div class="login-box">
+            <img src="../img/hero-img3.png" alt="Logo Aurana" class="logo_aurana"> 
+                <h2>CONNEXION</h2>
+                <form>
+                <div class="user-box">
+                    <input type="text" name="" required="">
+                    <label>Username</label>
+                </div>
+                <div class="user-box">
+                    <input type="password" name="" required="">
+                    <label>Password</label>
+                </div>
+                <div class="remember-me">
+                    <input type="checkbox" id="remember">
+                    <label for="remember">Se souvenir de moi</label>
+                    </div>
+                    <!-- <button>RAVI DE TE REVOIR GRENOUILLE</button> -->
+                    <button class="grenouille-button">RAVI DE TE REVOIR GRENOUILLE</button>
+                    <div class="forgot-password">
+                    <a href="#">Mot de passe oublié ?</a>
+                    </div>
+        </div>
+    </section>
+</body>
+</html>
